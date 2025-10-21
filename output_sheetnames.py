8855bbf52c4027d3a0a12f74a651ad1291032585
Wouter Schoenmakers <w.k.schoenmakers@gmail.com>
2025-10-21 16:03:54 +0200

    switch

--- output_sheetnames.py
+++ output_sheetnames.py
@@ -3,7 +3,7 @@ from enum import Enum
 class SHEETS_OUT(Enum):
     SHEET_TOEL = "Toelichting invulsheet"
     SHEET_INVUL = "Invulsheet"
-    SHEET_TABEL = "Type Taxonomie"
-    SHEET_ATTR = "Attributen"
+    SHEET_TABEL = "Attributen"
+    SHEET_ATTR = "Type Taxonomie"
     SHEET_VAR = "Variabelen"
     SHEET_DOM = "Domeinwaarden"
